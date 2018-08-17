ad91f993cec90973d296d4c1d328367af8cdaa42
yufangyang <975057282@qq.com>
2018-08-17 17:26:19 +0800

    build

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,7 +11,9 @@ export function getQuery(search,name) {
 export function getApiUrl(search,name) {
   let host=window.location.host;
   if(/moapi\./.test(host)){
-    return window.location.protocol+"//api.yfyld.top:3030"
+    return window.location.protocol+"//api.yfyld.online:3030"
+  }else if(/97\./.test(host)){
+    return "http://97.64.36.18:3014"
   }else{
     return "http://127.0.0.1:3014"
   }
