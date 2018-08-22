f7acdddb5cb25686258c22d2d6aa05e29238d33b
yufangyang <975057282@qq.com>
2018-08-22 14:35:48 +0800

    build

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,11 +10,13 @@ export function getQuery(search,name) {
 
 export function getApiUrl(search,name) {
   let host=window.location.host;
-  if(/moapi\./.test(host)){
+  if(/91jkys\./.test(host)){
+    return window.location.protocol+"//mock.qa.91jkys.com/api"
+  }else if(/moapi\./.test(host)){
     return window.location.protocol+"//api.yfyld.online:3030"
   }else if(/97\./.test(host)){
-    return "http://97.64.36.18:3014"
+    return "http://97.64.36.18:9215"
   }else{
-    return "http://127.0.0.1:3014"
+    return "http://127.0.0.1:9215"
   }
 }
