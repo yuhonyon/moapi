edad6a77e7726ef0e598a844b9b2981786ed53e3
yufangyang <975057282@qq.com>
2019-04-04 16:06:18 +0800

    dis cache

--- src/store/project.js
+++ src/store/project.js
@@ -73,7 +73,7 @@ class Project {
   }
   @computed
   get docUrl() {
-    return Config.baseURL + 'project/doc/' + this.projectId
+    return Config.baseURL + 'project/doc/' + this.projectId+'?cache=false'
   }
 
   @computed
