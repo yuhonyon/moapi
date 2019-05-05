3c26668e008be5debb1a6977896b528c2b0cf99b
yufangyang <975057282@qq.com>
2019-05-05 14:36:43 +0800

    build

--- src/routes/project/components/EditableTable.js
+++ src/routes/project/components/EditableTable.js
@@ -180,7 +180,6 @@ class EditableTable extends React.Component{
     title: '名称',
     dataIndex: 'name',
     key: 'name',
-    className: Style.flex,
     render:(text,record)=>this.renderColumns(text,record,'name')
   }, {
     title: '类型',
